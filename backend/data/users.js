915f43ac69777b9a481252ebc319687219302e32
Chan Li Liao <cliao119@gmail.com>
2021-01-07 19:35:16 -0500

    set up seeder

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,6 +2,12 @@ import bcrypt from 'bcryptjs';
 
 const users = [
   {
+    name: 'Henry Liao',
+    email: 'cliao119@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
     name: 'bob',
     email: 'bob@gmail.com',
     password: bcrypt.hashSync('123456', 10),
@@ -11,12 +17,6 @@ const users = [
     email: 'john@gmail.com',
     password: bcrypt.hashSync('123456', 10),
   },
-  {
-    name: 'Henry Liao',
-    email: 'cliao119@gmail.com',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
-  },
 ];
 
-export default bcrypt;
+export default users;
