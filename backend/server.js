3555a5bbf0c52fb09eb8760a1e6cb1849449d382
Chan Li Liao <cliao119@gmail.com>
2021-01-13 21:47:11 -0500

    create get user route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -11,14 +12,17 @@ connectDB();
 
 const app = express();
 
+//allow to us to accept json body for auth
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
