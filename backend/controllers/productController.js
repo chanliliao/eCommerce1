8d74e7c39b89091ae2de2f16c5a3c66e78806326
Chan Li Liao <cliao119@gmail.com>
2021-01-27 23:11:26 -0500

    endpoint done for edit and add new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -39,4 +39,54 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+//@desc   update product
+//@route  PUT /api/products/:id
+//@access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = req.body.name || product.name;
+    product.description = req.body.description || product.description;
+    product.brand = req.body.brand || product.brand;
+    product.image = req.body.image || product.image;
+    product.category = req.body.category || product.category;
+    product.price = req.body.price || product.price;
+    product.countInStock = req.body.countInStock || product.countInStock;
+
+    const updateProduct = await product.save();
+
+    res.json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+//@desc   Create Product
+//@route  POST /api/products/
+//@access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/image/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+};
